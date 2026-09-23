c27217e48f569338120054b6f244bab7070dbf83
cnhhryp-pixel <cnhhryp@gmail.com>
2026-09-23 18:11:19 +0800

    Add PDF split function module entry

---
+++ tools/pdf-split/app.js
@@ -0,0 +1,7 @@
+// PDF Split browser module placeholder
+// Next version will integrate pdf-lib for local page extraction.
+(function(){
+  window.splitPDF = function(file, pages){
+    console.log('PDF split module ready', file, pages);
+  };
+})();
